2fe8399b1571c4dae8a11a007057178b8b2773c8
kkpathak2000 <kkpathak.2000@gmail.com>
2025-02-02 18:17:03 +0530

    Refactor home section styling and structure; enhance image overlay and description layout

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,19 +4,19 @@ import homeImage from '../assets/home.png';
 const Home = () => {
   return (
     <section id="home" className="home-section">
-      <img src={homeImage} alt="home page" className="home-image" />
-      <div className="home-text">
-        <h1>Welcome to VidyaOne Tech</h1>
+      <div className="home-image-container">
+        <img src={homeImage} alt="home page" className="home-image" />
+        <div className="home-text-overlay">
+          <h1 className='home-heading'>Welcome to VidyaOne Tech</h1>
+        </div>
       </div>
-        <p className="home-description">
-          lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </p>
+
+      <p className="home-description">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </p>
 
       <hr className="home-hr" />
-      
     </section>
   );
 };
