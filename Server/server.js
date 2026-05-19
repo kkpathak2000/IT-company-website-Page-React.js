99b49194750c01a42764ba320462a7de213a23b1
Krishna Kant Pathak <kkpathak.2000@gmail.com>
2026-05-19 22:32:04 +0000

    Upgrade server to Express 5 and switch to built-in express body parsing

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 5000;
@@ -9,7 +8,7 @@ const PORT = 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 //MySQL Connection
 const db = mysql.createConnection({
